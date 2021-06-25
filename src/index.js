12b9ebe270266123e99020d01a7f57e8c4bdc938
Ricardo <ricarditomontserrat@gmail.com>
2021-06-25 22:56:57 +0200

    Finally Bootstrap Theming

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from 'react-service-worker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
